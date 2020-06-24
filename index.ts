bb65fee9810bced479b1037ea5f5ddb29a87366a
Luke Dawson <l.j.dwsn@gmail.com>
2020-06-24 10:21:43 +1000

    Changing to desktop

--- index.ts
+++ index.ts
@@ -39,26 +39,241 @@ export const someLexFunction = new aws.lambda.CallbackFunction(
   {
     callback: async (event: LexEvent) => {
       let { slots, name } = event.currentIntent;
-
-      await asyncPost();
+      asyncPost();
 
       (async function () {
         let response = await asyncGet();
-        if (response.data.shop != null) {
-          return {
-            dialogAction: {
-              type: "ElicitSlot",
-              intentName: name,
-              slotToElicit: "sync_evaluation",
-              slots,
-              message: {
-                contentType: "CustomPayload",
-                content: "Success ;)",
-              },
-            },
-          };
-        } else return console.log("hi");
+        if (response.data.shop != null)
+          console.log("Your store is " + response.data.shop);
+        else console.log("You do not have a shop");
+        if (response.data.shop != null)
+          console.log("Your store is " + response.data.shop);
+        else console.log("You do not have a shop");
+        if (response.data.blogCount != null)
+          console.log("You have " + response.data.blogCount + " blog posts");
+        else console.log("You do not have any blogs");
+        if (response.data.availableBlogCount != null)
+          console.log(
+            "You have " + response.data.availableBlogCount + " blog posts"
+          );
+        else console.log("You don't have any available blogs");
+        if (response.data.relatedPostCount != null)
+          console.log(
+            "You have " + response.data.relatedPostCount + " related blog posts"
+          );
+        else console.log("You do not have any available blogs");
+        if (response.data.relatedPostCount != null)
+          console.log(
+            "You have " +
+              response.data.relatedProductCount +
+              " related products"
+          );
+        else console.log("You do not have any related products");
       })();
+
+      if (slots.debugging_clarification === "yes") {
+        return {
+          dialogAction: {
+            type: "Close",
+            fulfillmentState: "Fulfilled",
+            message: {
+              contentType: "PlainText",
+              content:
+                "Good to hear! Let me know if I can help you with anything else",
+            },
+            responseCard: {
+              version: "0",
+              contentType: "application/vnd.amazonaws.card.generic",
+              genericAttachments: [
+                {
+                  buttons: [
+                    {
+                      text: "further help",
+                      value: "options",
+                    },
+                    {
+                      text: "Give us feedback!",
+                      value: "feedback",
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
+
+      if (slots.sync_evaluation === "yes") {
+        return {
+          dialogAction: {
+            type: "Close",
+            fulfillmentState: "Fulfilled",
+            message: {
+              contentType: "PlainText",
+              content: "What is the problem you are encountering?",
+            },
+            responseCard: {
+              version: "0",
+              contentType: "application/vnd.amazonaws.card.generic",
+              genericAttachments: [
+                {
+                  buttons: [
+                    {
+                      text: "I want my product pages to show up on my blog",
+                      value: "products",
+                    },
+                    {
+                      text: "How do I use tags with Blog Linker",
+                      value: "tags",
+                    },
+                    {
+                      text:
+                        "How do I make my related products to populate on my blog posts",
+                      value: "populate",
+                    },
+                    {
+                      text: "My images and text look wrong ",
+                      value: "css",
+                    },
+                    {
+                      text: "More options",
+                      value: "more",
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
+
+      if (slots.sync_evaluation === "no") {
+        return {
+          dialogAction: {
+            type: "ElicitSlot",
+            intentName: name,
+            slotToElicit: "options",
+            slots,
+
+            responseCard: {
+              version: "0",
+              contentType: "application/vnd.amazonaws.card.generic",
+              genericAttachments: [
+                {
+                  buttons: [
+                    {
+                      text: "I want my product pages to show up on my blog",
+                      value: "products",
+                    },
+                    {
+                      text: "How do I use tags with Blog Linker",
+                      value: "tags",
+                    },
+                    {
+                      text:
+                        "How do I make my related products to populate on my blog posts",
+                      value: "populate",
+                    },
+                    {
+                      text: "My images and text look wrong ",
+                      value: "css",
+                    },
+                    {
+                      text: "More options",
+                      value: "more",
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
+
+      if (slots.further_options === "more") {
+        return {
+          dialogAction: {
+            type: "ElicitSlot",
+            intentName: name,
+            slotToElicit: "final_options",
+            slots,
+
+            responseCard: {
+              version: "0",
+              contentType: "application/vnd.amazonaws.card.generic",
+              genericAttachments: [
+                {
+                  buttons: [
+                    {
+                      text: "Option 5",
+                      value: "products",
+                    },
+                    {
+                      text: "Option 6",
+                      value: "tags",
+                    },
+                    {
+                      text: "Option 7",
+                      value: "populate",
+                    },
+                    {
+                      text: "Option 8 ",
+                      value: "css",
+                    },
+                    {
+                      text: "More options",
+                      value: "more",
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
+
+      if (slots.options === "more") {
+        return {
+          dialogAction: {
+            type: "ElicitSlot",
+            intentName: name,
+            slotToElicit: "further_options",
+            slots,
+
+            responseCard: {
+              version: "0",
+              contentType: "application/vnd.amazonaws.card.generic",
+              genericAttachments: [
+                {
+                  buttons: [
+                    {
+                      text: "I want my product pages to show up on my blog",
+                      value: "products",
+                    },
+                    {
+                      text: "How do I use tags with Blog Linker",
+                      value: "tags",
+                    },
+                    {
+                      text:
+                        "How do I make my related products to populate on my blog posts",
+                      value: "populate",
+                    },
+                    {
+                      text: "My images and text look wrong ",
+                      value: "css",
+                    },
+                    {
+                      text: "More options",
+                      value: "more",
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
 
       return {
         dialogAction: {
